bbfdb061741c1815e641a39207adae16266784b5
Anggia Maisa Hadaya <anggimhadaya@gmail.com>
2019-03-13 23:12:35 +0700

    Starting with workbox

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,28 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.0/workbox-sw.js');
 const CACHE_NAME = "football-v9";
+var base_url = "https://api.football-data.org/";
+
+workbox.precaching.precacheAndRoute([
+    { url: '/', revision: '1' },
+    { url: '/index.html', revision: '1' },
+    { url: '/manifest.json', revision: '1' },
+    { url: '/detailMatch.html', revision: '1' },
+    { url: '/favicon.ico', revision: '1' },
+    { url: '/nav.html', revision: '1' },
+    { url: '/css/materialize.css', revision: '1' },
+    { url: '/css/materialize.min.css', revision: '1' },
+    { url: '/js/materialize.min.js', revision: '1' },
+    { url: '/js/materialize.js', revision: '1' },
+    { url: '/js/script.js', revision: '1' },
+    { url: '/js/nav.js', revision: '1' },
+    { url: '/js/api.js', revision: '1' },
+    { url: '/js/db.js', revision: '1' },
+    { url: '/js/idb.js', revision: '1' },
+    { url: '/js/main.js', revision: '1' },
+    { url: '/img/icon.png', revision: '1' }
+]);
+
+
 var urlToCache = [
     "/",
     "/manifest.json",
@@ -19,6 +43,21 @@ var urlToCache = [
     "/js/script.js",
     "/img/icon.png"
 ];
+
+workbox.routing.registerRoute(
+    new RegExp('/pages/'),
+    workbox.strategies.staleWhileRevalidate()
+);
+
+workbox.routing.registerRoute(
+    new RegExp(base_url + "v2/competitions/CL/matches"),
+    workbox.strategies.staleWhileRevalidate()
+);
+
+workbox.routing.registerRoute(
+    new RegExp(base_url + "v2/matches/"),
+    workbox.strategies.staleWhileRevalidate()
+);
 
 self.addEventListener("install", function (event) {
     event.waitUntil(
